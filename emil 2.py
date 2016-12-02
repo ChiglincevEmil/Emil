d3772fab4e1c05c5082fa893a802b76d4abd6e64
agamestra <shyshygon@ya.ru>
2016-12-02 14:52:37 +0300

    laba 13/6

--- emil 2.py
+++ emil 2.py
@@ -1,15 +1,17 @@
 import turtle as t
 
-def curve( l , n):
+def curve( l , n, an=45):
     if n == 0:
         t.forward(l)
     else:
-        x= l/n**0/5
-        t.left(45)
-        curve(x,n-1)
-        t.right(90)
-        curve(x,n-1)
-        t.left(45)
-curve(4000,3)
+        x= l/1.6
+        t.left(an)
+        curve(x,n-1, 45)
+        t.right(an*2)
+        curve(x,n-1, -45)
+        t.left(an)
+
+
+curve(400000,20)
 
 t.speed("slowest")
